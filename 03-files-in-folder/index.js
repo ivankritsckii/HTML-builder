a1dab6d8dc68875f13563375bc8eb5e2bd3d8f15
Ivan Kritsckii <ivan.kritsckii@yandex.ru>
2024-01-19 10:21:21 +0500

    fix: solve 3 test

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,6 +1,23 @@
-require('colors');
+let fs = require('fs');
+let path = require('path');
 
 
-const text = "Hello, world!";
-console.log(text.rainbow);
+let dir = path.join(__dirname, 'secret-folder');
+fs.readdir(dir, {withFileTypes: true}, (err, files) => {
+    let arr = files.map((item) => {
+        if(item.isFile()){
+            return item
+        }})
+    arr = arr.filter((i) => !!i);
+    let arrNames = arr.map((item) =>{
+        return item.name.split('.')
+    });
+    let arrSize = arr.map((item, index) =>{
+        fs.stat(path.join(dir, item.name), (err, stats) =>{
+            console.log(`${arrNames[index][0]} - ${arrNames[index][arrNames[index].length - 1]} -${stats.size / 1000}kb`);
+            return 1
+        });
+    })
+});
+
 
